88548e82764288d92877f7a9c22b631f4298f308
Gorkem Madran <gorkemmadran@hotmail.com>
2023-12-22 19:45:16 +0300

    işlem bitti

---
+++ app.js
@@ -0,0 +1,27 @@
+const screen = document.querySelector('.input');
+const buttons = document.querySelectorAll('button');
+const clear = document.querySelector('.clear');
+const equal = document.querySelector('.equal');
+let curVal;
+
+
+function calculator(){
+buttons.forEach((element) =>{
+element.addEventListener('click', (e) =>{
+    if(e.target.innerText == '='){
+        curVal = String(eval(curVal));
+        screen.value = curVal;
+
+    }else if(e.target.innerText == 'AC'){
+        curVal = '';
+        screen.value = curVal;
+    }
+    else{
+        curVal += e.target.innerText;
+        screen.value = curVal;
+    }
+})
+})
+}
+
+calculator()
